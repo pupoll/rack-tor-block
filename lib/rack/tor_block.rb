6f3118051e457b1a171556936bb5bd37d706390a
Andreas Schacke <andreas.schacke@gmail.com>
2014-05-15 10:00:35 +0200

    . final version

--- lib/rack/tor_block.rb
+++ lib/rack/tor_block.rb
@@ -4,17 +4,25 @@ require 'rack/ip'
 module Rack
   class TorBlock
     
-    DEFAULT_REDIRECT = 'http://localhost:3000/no_js'
+    ERROR_PAGE = '/blocked_proxy'
     def initialize(app)
       @app = app
     end
 
     def call(env)
       req = Rack::Request.new(env)
-      return [302, {'Content-Type' => 'text/html', 'Location' => DEFAULT_REDIRECT}, [] ] if (Rack::IP.new(env['action_dispatch.remote_ip'] || req.ip).is_tor?) && req.url != DEFAULT_REDIRECT
+      return [302, {'Content-Type' => 'text/html', 'Location' => "#{req.scheme}://#{req.host_with_port}#{ERROR_PAGE}"}, [] ] if (Rack::IP.new(env['action_dispatch.remote_ip'] || req.ip).is_tor?) && !error_page?(req) && !asset?(req)
         
       #Normal processing
       @app.call(env)
+    end
+    
+    def error_page?(req)
+      req.url =~ /#{ERROR_PAGE}$/
+    end
+    
+    def asset?(req)
+      req.url =~ /\/assets\//
     end
   end
 end
