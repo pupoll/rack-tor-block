77828e2d614620d3c5465e1ec7d48831fcd95170
Andreas Schacke <andreas.schacke@gmail.com>
2014-05-15 09:12:25 +0200

    . fooling around

--- lib/rack/tor_block.rb
+++ lib/rack/tor_block.rb
@@ -4,15 +4,14 @@ require 'rack/ip'
 module Rack
   class TorBlock
     
-    DEFAULT_REDIRECT = 'https://sorry.google.com' #We're sorry from Google
-
+    DEFAULT_REDIRECT = 'http://localhost:3000/no_js'
     def initialize(app)
       @app = app
     end
 
     def call(env)
       req = Rack::Request.new(env)
-      return [302, {'Content-Type' => 'text', 'Location' => DEFAULT_REDIRECT}, [] ] if Rack::IP.new(env['action_dispatch.remote_ip'] || req.ip).is_tor?
+      return [302, {'Content-Type' => 'text/html', 'Location' => DEFAULT_REDIRECT}, [] ] if Rack::IP.new(env['action_dispatch.remote_ip'] || req.ip).is_tor?
         
       #Normal processing
       @app.call(env)
